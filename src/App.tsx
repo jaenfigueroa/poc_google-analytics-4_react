4ef4454ebde1c813b70c9f6c29eb41a5d9f35b75
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-02-20 17:17:58 -0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,37 @@ import Formulario from './components/Form'
 
 import ReactGA from 'react-ga4'
 
-// ReactGA.initialize('G-38ZPM6NFPD')
-
-// Multiple products (previously known as trackers)
-ReactGA.initialize([
-  {
-    trackingId: 'G-38ZPM6NFPD',
-    // gaOptions: {...}, // optional
-    // gtagOptions: {...}, // optional
-  },
-  // {
-  //   trackingId: 'your second GA measurement id',
-  // },
-])
-
-// Send pageview with a custom path
-ReactGA.send({
-  hitType: 'pageview',
-  page: '/',
-  // title: 'Custom Title',
-})
-
-// Send a custom event
-ReactGA.event({
-  category: 'your category',
-  action: 'your action',
-  // label: 'your label', // optional
-  value: 99, // optional, must be a number
-  nonInteraction: true, // optional, true/false
-  transport: 'xhr', // optional, beacon/xhr/image
-})
-
 const App = () => {
+  // ReactGA.initialize('G-38ZPM6NFPD')
+
+  // Multiple products (previously known as trackers)
+  ReactGA.initialize([
+    {
+      trackingId: 'G-38ZPM6NFPD',
+      // gaOptions: {...}, // optional
+      // gtagOptions: {...}, // optional
+    },
+    // {
+    //   trackingId: 'your second GA measurement id',
+    // },
+  ])
+
+  // Send pageview with a custom path
+  ReactGA.send({
+    hitType: 'pageview',
+    page: '/',
+    // title: 'Custom Title',
+  })
+
+  // Send a custom event
+  ReactGA.event({
+    category: 'your category',
+    action: 'your action',
+    // label: 'your label', // optional
+    value: 99, // optional, must be a number
+    nonInteraction: true, // optional, true/false
+    transport: 'xhr', // optional, beacon/xhr/image
+  })
   return (
     <div>
       {/* <Youtube /> */}
