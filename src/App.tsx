0ae68ba28b7bbb0b1b0f698c0a15e7df604346dd
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-02-20 16:17:47 -0500

    add react-ga4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import Formulario from './components/Form'
-import Youtube from './components/Youtube'
+// import Youtube from './components/Youtube'
+
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-38ZPM6NFPD')
+
+// Multiple products (previously known as trackers)
+// ReactGA.initialize([
+//   {
+//     trackingId: 'G-38ZPM6NFPD',
+//     // gaOptions: {...}, // optional
+//     // gtagOptions: {...}, // optional
+//   },
+//   {
+//     trackingId: 'your second GA measurement id',
+//   },
+// ])
+
+// Send pageview with a custom path
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+  title: 'Custom Title',
+})
+
+// Send a custom event
+// ReactGA.event({
+//   category: 'your category',
+//   action: 'your action',
+//   label: 'your label', // optional
+//   value: 99, // optional, must be a number
+//   nonInteraction: true, // optional, true/false
+//   transport: 'xhr', // optional, beacon/xhr/image
+// })
 
 const App = () => {
   return (
     <div>
-      <Youtube />
+      {/* <Youtube /> */}
       <Formulario />
     </div>
   )
