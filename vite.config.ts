7301f95f51b0580761d6acc7d68154427005f279
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-02-20 14:00:19 -0500

    deploy website

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true'
+
+const base = isGitHubActions ? '/poc_google-analytics-4_react' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })
